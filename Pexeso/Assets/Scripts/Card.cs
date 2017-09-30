ea178bf18ea1476bab5e492cb5a50c87df67a297
TomiukQT <TomiukQT@gmail.com>
2017-09-30 20:04:24 +0200

    pexeso 8*8 grid

---
+++ Pexeso/Assets/Scripts/Card.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int id;
+    public GameObject cardObj;
+
+
+
+    public void Start()
+    {
+      
+    }
+
+
+    public void Flip()
+    {
+        //anim.Play(flipCard.name);
+
+        ////if (!isStart)
+        ////{
+        ////    isStart = true;
+        ////    anim.SetBool("isStart", isStart);
+        ////}
+
+        ////if (isFlippedBack)
+        ////    isFlippedBack = false;
+        ////else
+        ////    isFlippedBack = true;
+
+        ////anim.SetBool("isFlippedBack", isFlippedBack);
+
+        ////Debug.Log("Flipped");
+    }
+
+    private void OnMouseDown()
+    {
+        Flip();
+    }
+
+
+}
---
+++ Pexeso/Assets/Scripts/GameMaker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaker : MonoBehaviour {
+
+    public GameObject card;
+    public List<GameObject> cards = new List<GameObject>();
+    public List<Material> materials = new List<Material>();
+
+    //gameplay
+    private int random;
+
+    //graphics
+    public float offset = 0.1f;
+
+	
+	void Start ()
+    {
+        GenerateCardField();
+        GenerateCards();        
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+   
+
+
+    private void GenerateCardField()
+    {
+
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                //TODO: Choose from list of cards
+                Vector3 dir = new Vector3(x + offset*x, y + offset*y,0 );                
+                cards.Add(Instantiate(card, dir, Quaternion.identity));
+            }
+        }
+
+
+    }
+    private void GenerateCards()
+    {       
+            for (int i = 0; i < 64; i++)
+            {
+                random = Random.Range(0, cards.Count+1);
+                cards[random].GetComponent<Card>().id = i;
+                
+            }
+        
+    }
+
+}
